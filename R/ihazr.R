2783e66cb254cedc8637594f60e0a3e5fd0c2e23
C. Jason Liang <liangcj@gmail.com>
2015-08-11 15:13:02 -0700

    initial commit
    just htmlwidget skeleton files, plus d3 library and my css style file

---
+++ R/ihazr.R
@@ -0,0 +1,38 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+ihazr <- function(message, width = NULL, height = NULL) {
+
+  # forward options using x
+  x = list(
+    message = message
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'ihazr',
+    x,
+    width = width,
+    height = height,
+    package = 'ihazr'
+  )
+}
+
+#' Widget output function for use in Shiny
+#'
+#' @export
+ihazrOutput <- function(outputId, width = '100%', height = '400px'){
+  shinyWidgetOutput(outputId, 'ihazr', width, height, package = 'ihazr')
+}
+
+#' Widget render function for use in Shiny
+#'
+#' @export
+renderIhazr <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  shinyRenderWidget(expr, ihazrOutput, env, quoted = TRUE)
+}
